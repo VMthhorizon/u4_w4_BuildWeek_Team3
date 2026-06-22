91c4d42817878e494b25bd2ba5bc09f3819c235b
Vincenzo Mola <vincenzo.mola@hotmail.com>
2026-06-22 10:54:09 +0200

    Initial commit

---
+++ src/main/java/Team3/Application.java
@@ -0,0 +1,13 @@
+package Team3;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Application {
+
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("u4buildweekpu");
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
