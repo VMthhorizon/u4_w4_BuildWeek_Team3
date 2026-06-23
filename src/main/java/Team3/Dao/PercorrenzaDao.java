c25b144a0a9d5f5a0b7ddc9a1286116746cd2bd9
Kevin Donati <91902874+kevindonati@users.noreply.github.com>
2026-06-23 19:36:41 +0200

    tempo medio e count

--- src/main/java/Team3/Dao/PercorrenzaDao.java
+++ src/main/java/Team3/Dao/PercorrenzaDao.java
@@ -1,11 +1,15 @@
 package Team3.Dao;
 
+import Team3.entities.MezzoDiTrasporto;
 import Team3.entities.Percorrenza;
+import Team3.entities.StoricoMezzo;
 import Team3.entities.TitoloViaggio;
+import Team3.enums.StatoMezzo;
 import Team3.exceptions.NotFoundException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
 
+import java.util.List;
 import java.util.UUID;
 
 public class PercorrenzaDao {
@@ -29,11 +33,27 @@ public class PercorrenzaDao {
         return newPercorrenza;
     }
 
-    //FIND BY ID
+    // FIND BY ID
     public Percorrenza findById(String id) {
         Percorrenza percorrenzaTrovata = this.entityManager.find(Percorrenza.class, UUID.fromString(id));
         if (percorrenzaTrovata == null) throw new NotFoundException("Impossibile trovare la percorrenza con id " + id);
         System.out.println("La percorrenza con id: " + id + " é stata trovata");
         return percorrenzaTrovata;
+    }
+
+    // NUMERO CORSE
+    public Long countNumeroCorse(UUID idMezzo, UUID idTratta) {
+        return entityManager.createQuery("SELECT COUNT(p) FROM Percorrenza p WHERE p.mezzoDiTrasporto.id_mezzo = :idMezzo AND p.tratta.idTratta = :idTratta", Long.class)
+                .setParameter("idMezzo", idMezzo)
+                .setParameter("idTratta", idTratta)
+                .getSingleResult();
+    }
+
+    // TEMPO MEDIO EFFETTIVO PERCORRENZA
+    public Double AverageTempoPercorenza(UUID idMezzo, UUID idTratta) {
+        return entityManager.createQuery("SELECT AVG(p.tempoEffettivo) FROM Percorrenza p WHERE p.mezzoDiTrasporto.id_mezzo = :idMezzo AND p.tratta.idTratta = :idTratta", Double.class)
+                .setParameter("idMezzo", idMezzo)
+                .setParameter("idTratta", idTratta)
+                .getSingleResult();
     }
 }
